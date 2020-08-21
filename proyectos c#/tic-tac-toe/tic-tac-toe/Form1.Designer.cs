2a1eb0de5d2ed9fa878b90c374b6867427f2c0e3
knlc01 <43591384+knlc01@users.noreply.github.com>
2020-08-21 12:23:06 -0300

    Add files via upload

---
+++ proyectos c#/tic-tac-toe/tic-tac-toe/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.tb_principal = new System.Windows.Forms.TextBox();
+            this.b_reset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(194, 67);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(100, 100);
+            this.a1.TabIndex = 0;
+            this.a1.Text = "-";
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(300, 67);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(100, 100);
+            this.a2.TabIndex = 1;
+            this.a2.Text = "-";
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(406, 67);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(100, 100);
+            this.a3.TabIndex = 2;
+            this.a3.Text = "-";
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(194, 173);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 3;
+            this.b1.Text = "-";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(300, 173);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 4;
+            this.b2.Text = "-";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(406, 173);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 5;
+            this.b3.Text = "-";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // c1
+            // 
+            this.c1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(194, 279);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(100, 100);
+            this.c1.TabIndex = 6;
+            this.c1.Text = "-";
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.c1_Click);
+            // 
+            // c2
+            // 
+            this.c2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(300, 279);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(100, 100);
+            this.c2.TabIndex = 7;
+            this.c2.Text = "-";
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // c3
+            // 
+            this.c3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(406, 279);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(100, 100);
+            this.c3.TabIndex = 8;
+            this.c3.Text = "-";
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // tb_principal
+            // 
+            this.tb_principal.Enabled = false;
+            this.tb_principal.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_principal.Location = new System.Drawing.Point(12, 67);
+            this.tb_principal.Multiline = true;
+            this.tb_principal.Name = "tb_principal";
+            this.tb_principal.Size = new System.Drawing.Size(164, 312);
+            this.tb_principal.TabIndex = 9;
+            // 
+            // b_reset
+            // 
+            this.b_reset.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_reset.Location = new System.Drawing.Point(556, 67);
+            this.b_reset.Name = "b_reset";
+            this.b_reset.Size = new System.Drawing.Size(230, 45);
+            this.b_reset.TabIndex = 10;
+            this.b_reset.Text = "RESETEAR";
+            this.b_reset.UseVisualStyleBackColor = true;
+            this.b_reset.Click += new System.EventHandler(this.b_reset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(556, 130);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 249);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.b_reset);
+            this.Controls.Add(this.tb_principal);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.TextBox tb_principal;
+        private System.Windows.Forms.Button b_reset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ proyectos c#/tic-tac-toe/tic-tac-toe/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+
+        string jugador;
+        int turnos = 0;
+        bool gano = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            jugar();
+        }
+
+        void jugar()
+        {
+
+            if (turnos < 9)
+            {
+                ganador();
+                if (turnos % 2 == 0)
+                {
+                    jugador = "X";
+                }
+                else
+                {
+                    jugador = "O";
+                }
+                tb_principal.Text = tb_principal.Text + "Turno de jugador " + jugador + "\r\n";
+                turnos = turnos + 1;
+            }
+            if (turnos == 10 && !gano)
+            {
+                MessageBox.Show("empate", "fin del juego");
+            }
+
+        }
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+            if (a1.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    a1.Text = "X";
+                    a1.ForeColor = Color.Red;
+                }
+                else
+                {
+                    a1.Text = "O";
+                    a1.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+            if (a2.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    a2.Text = "X";
+                    a2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    a2.Text = "O";
+                    a2.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+            if (a3.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    a3.Text = "X";
+                    a3.ForeColor = Color.Red;
+                }
+                else
+                {
+                    a3.Text = "O";
+                    a3.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (b1.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    b1.Text = "X";
+                    b1.ForeColor = Color.Red;
+                }
+                else
+                {
+                    b1.Text = "O";
+                    b1.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            if (b2.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    b2.Text = "X";
+                    b2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    b2.Text = "O";
+                    b2.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            if (b3.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    b3.Text = "X";
+                    b3.ForeColor = Color.Red;
+                }
+                else
+                {
+                    b3.Text = "O";
+                    b3.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void c1_Click(object sender, EventArgs e)
+        {
+            if (c1.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    c1.Text = "X";
+                    c1.ForeColor = Color.Red;
+                }
+                else
+                {
+                    c1.Text = "O";
+                    c1.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+            if (c2.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    c2.Text = "X";
+                    c2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    c2.Text = "O";
+                    c2.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+            }
+        }
+
+        private void c3_Click(object sender, EventArgs e)
+        {
+            if (c3.Text == "-")
+            {
+                if (jugador == "X")
+                {
+                    c3.Text = "X";
+                    c3.ForeColor = Color.Red;
+                }
+                else
+                {
+                    c3.Text = "O";
+                    c3.ForeColor = Color.Green;
+                }
+                jugar();
+            }
+            else
+            {
+                MessageBox.Show("esta casilla ya esta ocupada" + jugador, "advertencia");
+                tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+            }
+        }
+
+        void ganador()
+        {
+            if (a1.Text == a2.Text && a2.Text == a3.Text)
+            {
+                if (a1.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+            if (b1.Text == b2.Text && b2.Text == b3.Text)
+            {
+                if (b1.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+            if (c1.Text == c2.Text && c2.Text == c3.Text)
+            {
+                if (c1.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+
+            //------------------------------------
+
+            if (a1.Text == b1.Text && b1.Text == c1.Text)
+            {
+                if (a1.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+            if (a2.Text == b2.Text && b2.Text == c2.Text)
+            {
+                if (a2.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+            if (a3.Text == b3.Text && b3.Text == c3.Text)
+            {
+                if (a3.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+
+            //-----------------------------
+
+            if (a1.Text == b2.Text && b2.Text == c3.Text)
+            {
+                if (a1.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+            if (a3.Text == b2.Text && b2.Text == c1.Text)
+            {
+                if (a3.Text != "-")
+                {
+                    MessageBox.Show("gano " + jugador, "ganador");
+                    tb_principal.Text = tb_principal.Text + "gano el jugador" + jugador + "\r\n";
+                    gano = true;
+                    desabilita();
+                }
+            }
+
+        }
+
+        void desabilita()
+        {
+            a1.Enabled = false;
+            a2.Enabled = false;
+            a3.Enabled = false;
+            b1.Enabled = false;
+            b2.Enabled = false;
+            b3.Enabled = false;
+            c1.Enabled = false;
+            c2.Enabled = false;
+            c3.Enabled = false;
+        }
+
+        void reset()
+        {
+            a1.Enabled = true;
+            a1.Text = "-";
+            a1.ForeColor = Color.Black;
+            a2.Enabled = true;
+            a2.Text = "-";
+            a2.ForeColor = Color.Black;
+            a3.Enabled = true;
+            a3.Text = "-";
+            a3.ForeColor = Color.Black;
+            b1.Enabled = true;
+            b1.Text = "-";
+            b1.ForeColor = Color.Black;
+            b2.Enabled = true;
+            b2.Text = "-";
+            b2.ForeColor = Color.Black;
+            b3.Enabled = true;
+            b3.Text = "-";
+            b3.ForeColor = Color.Black;
+            c1.Enabled = true;
+            c1.Text = "-";
+            c1.ForeColor = Color.Black;
+            c2.Enabled = true;
+            c2.Text = "-";
+            c2.ForeColor = Color.Black;
+            c3.Enabled = true;
+            c3.Text = "-";
+            c3.ForeColor = Color.Black;
+
+            turnos = 0;
+
+            tb_principal.Text = "";
+
+        }
+
+        private void b_reset_Click(object sender, EventArgs e)
+        {
+            reset();
+            jugar();
+        }
+
+
+    }
+}
